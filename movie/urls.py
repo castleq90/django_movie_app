5c64f46b89477e5b3eef5784d37a3e03548eba42
castleq90 <castleq37@gmail.com>
2021-12-07 04:59:35 +0900

    [Add] drf swagger

--- movie/urls.py
+++ movie/urls.py
@@ -16,7 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movie/', include('movies.urls'))
+    path('movie/', include('movies.urls')),
+    path(r'swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
