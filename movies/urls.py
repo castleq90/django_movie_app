15f6c05d9f36681ae2827f17a56fd4bb6f4db28e
castleq90 <castleq37@gmail.com>
2021-12-07 04:22:14 +0900

    [Add] review 생성 수정 삭제 기능 완료

--- movies/urls.py
+++ movies/urls.py
@@ -4,4 +4,7 @@ from movies import views
 urlpatterns =[
     path('list/', views.MovieList.as_view()),
     path('detail/<int:pk>/',views.MovieDetail.as_view()),
+    path('review/<int:pk>/',views.MovieReviewDetail.as_view()),
+    path('review/',views.MovieReviewCreate.as_view()),
+
 ]
