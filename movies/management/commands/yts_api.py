f3193d3f6f02a7b2888eb30d5c1ce3b115a37dc2
castleq90 <castleq37@gmail.com>
2021-12-06 23:12:34 +0900

    [Add] api 데이터 삽입

---
+++ movies/management/commands/yts_api.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+from movies.models import Movie
+
+# api_url = 'https://yts.mx/api/v2/list_movies.json?sort_by=download_count&limit=50'
+# data = requests.get(api_url)
+# json_data = json.loads(data.text)
+
+# for data in json_data['data']['movies']:
+#     Movie.objects.create(
+#         title   = data.get('title'),
+#         year    = data.get('year'),
+#         rating  = data.get('rating'),
+#         genres  = data.get('genres'),
+#         summary = data.get('summary'),
+#     )
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        api_url = 'https://yts.mx/api/v2/list_movies.json?sort_by=download_count&limit=50'
+        data = requests.get(api_url)
+        json_data = json.loads(data.text)
+
+        for data in json_data['data']['movies']:
+            Movie.objects.create(
+                title   = data.get('title'),
+                year    = data.get('year'),
+                rating  = data.get('rating'),
+                genres  = data.get('genres'),
+                summary = data.get('summary'),
+            )
